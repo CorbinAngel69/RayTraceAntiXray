a432ab3c6d0b66cc5ee69c5b6dad4bc10156f8ec
CorbinAngel69 <corbinangel460@gmail.com>
2025-12-29 08:50:54 +0500

    Delete settings.gradle.kts

--- settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-rootProject.name = "raytraceantixray"
-
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven("https://repo.papermc.io/repository/maven-public/")
-    }
-}
